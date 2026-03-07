0160f4aa662ccc9b0fa004640f97713ff218eb00
Prince Randy M. Gonzales <princerandygonzales@gmail.com>
2026-03-07 11:53:23 +0800

    ggg

--- backend/src/queues/redisOption.ts
+++ backend/src/queues/redisOption.ts
@@ -1,10 +1,13 @@
+
+// If REDIS_URL is set, use it directly (for Upstash, etc.)
 import { RedisOptions } from "ioredis";
 
-export const redisOptions: RedisOptions = {
-  host: process.env.REDIS_HOST || "redis-server", // Upstash or Docker Compose
-  port: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : 6379,
-  password: process.env.REDIS_PASSWORD || undefined,
-  maxRetriesPerRequest: null, // ✅ required for BullMQ workers
-};
+export const redisOptions: RedisOptions | string =
+  process.env.REDIS_URL || {
+    host: process.env.REDIS_HOST || "redis",
+    port: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : 6379,
+    password: process.env.REDIS_PASSWORD || undefined,
+    maxRetriesPerRequest: null,
+  };
 //Use "redis" as the host because it's the service name from Docker Compose.
 // It acts as the hostname within the Docker network.
